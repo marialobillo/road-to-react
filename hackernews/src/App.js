2a235c2b6170047f8bdb30b367e978bc488e14e3
marialobillo <maria.lobillo.santos@gmail.com>
2018-09-03 13:47:19 +0200

    using axios instead of fetch

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 import Search from './components/Search';
 import Table from  './components/Table';
 import Button from './components/Button';
@@ -14,6 +15,9 @@ const PARAM_PAGE = 'page=';
 const PARAM_HPP = 'hitsPerPage=';
 
 class App extends Component {
+
+  _isMounted = false;
+
   constructor(props){
     super(props);
 
@@ -26,6 +30,7 @@ class App extends Component {
   }
 
   componentDidMount(){
+    this._isMounted = true;
     const { searchTerm } = this.state;
     this.setState({ searchKey: searchTerm });
     this.fetchSearchTopStories(searchTerm);
@@ -66,10 +71,9 @@ class App extends Component {
 
   fetchSearchTopStories = (searchTerm, page = 0) => {
     //console.log(this.state.searchTerm);
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
-      .then(response => response.json())
-      .then(result => this.setSearchTopStories(result))
-      .catch(error => this.setState({ error }));
+    axios(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+      .then(result => this._isMounted && this.setSearchTopStories(result.data))
+      .catch(error => this._isMounted && this.setState({ error }));
   }
 
   setSearchTopStories = (result) => {
@@ -94,6 +98,10 @@ class App extends Component {
 
   needsToSearchTopStories = (searchTerm) => {
     return !this.state.results[searchTerm];
+  }
+
+  componentWillUnmount(){
+    this._isMounted = false;
   }
 
   render() {

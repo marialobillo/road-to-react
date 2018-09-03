0ab5cf64652d78fb909aaa8cece08f9ba3217b64
marialobillo <maria.lobillo.santos@gmail.com>
2018-09-03 16:48:11 +0200

    testing App component

---
+++ hackernews/src/components/App/test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import App from '../App';
+import Search from '../Search';
+
+
+describe('Search', () => {
+
+  it('renders without crashing', () => {
+
+  });
+})
+
+
+describe('App', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(<App />);
+
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+  });
+});

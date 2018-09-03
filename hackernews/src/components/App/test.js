0923fc0d8b1eabe2e56a8f282a159ffe196b366e
marialobillo <maria.lobillo.santos@gmail.com>
2018-09-03 16:50:10 +0200

    tesitng Search component

--- hackernews/src/components/App/test.js
+++ hackernews/src/components/App/test.js
@@ -8,9 +8,18 @@ import Search from '../Search';
 describe('Search', () => {
 
   it('renders without crashing', () => {
-
+    const div = document.createElement('div');
+    ReactDOM.render(<Search>Search</Search>, div);
+    ReactDOM.unmountComponentAtNode(div);
   });
-})
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(<Search>Search</Search>);
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
 
 
 describe('App', () => {
